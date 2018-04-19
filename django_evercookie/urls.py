ae3f27a56e8eb5c9a373894b569051d5af0f4c9d
Roman Tolkachyov <roman@tolkachyov.name>
2018-04-19 17:04:23 +0300

    fix urlconf: force views import (for gevent)

--- django_evercookie/urls.py
+++ django_evercookie/urls.py
@@ -3,12 +3,20 @@ try:
 except ImportError:
     from django.conf.urls.defaults import url, patterns
 
+from django_evercookie.views import (
+    evercookie_cache,
+    evercookie_png,
+    evercookie_etag,
+    evercookie_core,
+    evercookie_auth,
+)
 """URLs differ from standart evercookie_<storage_method> to dodge easyprivacy blocking rules"""
 
 
 urlpatterns = patterns('django_evercookie.views',
-    url(r'^ec/cache', 'evercookie_cache', name='ecache'),
-    url(r'^ec/png', 'evercookie_png', name='epng'),
-    url(r'^ec/etag', 'evercookie_etag', name='ecetag'),
-    url(r'^ec/cookie', 'evercookie_core', name='ecookie'),
-    url(r'^ec/auth', 'evercookie_auth', name='ecauth'), )
+    url(r'^ec/cache', evercookie_cache, name='ecache'),
+    url(r'^ec/png', evercookie_png, name='epng'),
+    url(r'^ec/etag', evercookie_etag, name='ecetag'),
+    url(r'^ec/cookie', evercookie_core, name='ecookie'),
+    url(r'^ec/auth', evercookie_auth, name='ecauth'), 
+)
